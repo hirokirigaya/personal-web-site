61d2b214b9db27829922dca3b86a11f786dd74cf
hirokirigaya <privz28@gmail.com>
2023-01-14 13:42:44 -0300

    fix(date): fixed date

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,16 +9,19 @@ import {
   FiYoutube,
 } from "react-icons/fi";
 import * as Styled from "./styles";
-import { Link } from "react-router-dom";
 import ScrollReveal from "scrollreveal";
 import { GithubRepositoryInterface } from "../../interfaces/GithubRepository";
 import GitHubService from "../../services/GithubService";
 import { ContainerInfosHeader } from "../../components/reusable/containerInfos";
+import moment from "moment";
+import 'moment/dist/locale/pt-br'
+moment.locale("pt-br");
+
+const startedPromation = "2022-01-25";
 
 const Home = (): JSX.Element => {
   let Idade = new Date().getFullYear() - new Date("2002-07-19").getFullYear();
-  let ProgamationTime =
-    new Date().getMonth() - new Date("2020-01-25").getMonth();
+  let ProgamationTime = moment(startedPromation).fromNow(true);
   const title = useRef<HTMLDivElement>(null);
   const cardFirst = useRef<HTMLDivElement>(null);
   const titleSecondSection = useRef<HTMLDivElement>(null);
@@ -133,8 +136,8 @@ const Home = (): JSX.Element => {
             <div className="infos">
               <p className="about-me">
                 Olá possuo {Idade} anos e desenvolvo aplicações Web há{" "}
-                {ProgamationTime} meses, sempre visando a melhor experiência
-                para os usuários.
+                {ProgamationTime}, sempre visando a melhor experiência para os
+                usuários.
               </p>
 
               <div className="navgation-personal-web">
@@ -153,7 +156,10 @@ const Home = (): JSX.Element => {
           <a href="https://github.com/hirokirigaya" target="_blank">
             <FiGithub />
           </a>
-          <a href="https://www.youtube.com/channel/UCGanhUB_aetD16UBbUJ9n6g" target="_blank">
+          <a
+            href="https://www.youtube.com/channel/UCGanhUB_aetD16UBbUJ9n6g"
+            target="_blank"
+          >
             <FiYoutube />
           </a>
           <a href="mailto:danielferreiradeveloper@gmail.com">
@@ -168,10 +174,7 @@ const Home = (): JSX.Element => {
         </div>
       </Styled.FirstSection>
       <Styled.SecondSection>
-        <ContainerInfosHeader
-          ref={titleSecondSection}
-          id="projetos"
-        >
+        <ContainerInfosHeader ref={titleSecondSection} id="projetos">
           <p>Projetos</p>
           <h2>Projetos Recentes</h2>
         </ContainerInfosHeader>
@@ -184,7 +187,7 @@ const Home = (): JSX.Element => {
               {githubFinder &&
                 githubFinder.map((item) => (
                   <div key={item.id} className="item">
-                    <div className="content" >
+                    <div className="content">
                       <p className="title-project">Github Finder</p>
                       <p className="desc-project">
                         Página web onde é possivel pesquisar usuários do github
@@ -217,7 +220,7 @@ const Home = (): JSX.Element => {
                         Next JS + Styled Components + Typescrpit.
                       </p>
                     </div>
-                    <div className="link-repo" >
+                    <div className="link-repo">
                       <a href={item.html_url} target="_blank">
                         <FiArrowRight />
                       </a>
@@ -234,19 +237,35 @@ const Home = (): JSX.Element => {
         </div>
       </Styled.SecondSection>
       <Styled.ThirdSection>
-        <ContainerInfosHeader
-          ref={titleThirdSection}
-        >
+        <ContainerInfosHeader ref={titleThirdSection}>
           <p>Sobre</p>
           <h2>Quem sou?</h2>
         </ContainerInfosHeader>
 
         <div className="about-me">
           <p>
-            Meu nome é Daniel Ferreira, tenho {Idade} anos, nascido em Brasilia, desde pequeno sempre tive uma imensa paixão por tecnologia, principalmente informática e robótica, ao conhecer o desenvolvimento web resolvi pesquisar e aprender mais sobre, e acabou se tornando minha nova paixão, pois o fato de poder programar algo do zero e ver o resultado final é algo que me deixa muito feliz. Além de usar meus conhecimentos para ajudar o próximo venho buscado ajudar os programadores nas comunidades e no meu canal do youtube <a href="https://www.youtube.com/channel/UCGanhUB_aetD16UBbUJ9n6g">Daniel Ferreira</a>.
+            Meu nome é Daniel Ferreira, tenho {Idade} anos, nascido em Brasilia,
+            desde pequeno sempre tive uma imensa paixão por tecnologia,
+            principalmente informática e robótica, ao conhecer o desenvolvimento
+            web resolvi pesquisar e aprender mais sobre, e acabou se tornando
+            minha nova paixão, pois o fato de poder programar algo do zero e ver
+            o resultado final é algo que me deixa muito feliz. Além de usar meus
+            conhecimentos para ajudar o próximo venho buscado ajudar os
+            programadores nas comunidades e no meu canal do youtube{" "}
+            <a href="https://www.youtube.com/channel/UCGanhUB_aetD16UBbUJ9n6g">
+              Daniel Ferreira
+            </a>
+            .
           </p>
           <p>
-            Atualmente sou desenvolvedor web front end e estou cursando o 2° semestre de Análise e Desenvolvimento de Sistemas na Universidade Anhanguera. Tenho como objetivo me tornar um desenvolvedor full stack, e estou sempre buscando novos conhecimentos para alcançar o que almejo. Atualmente desenvolvo em Javascript/TS e como principal framework utilizo React JS, tenhos conhecimentos em estilizadores css, scss, styled-components, desenvolvimento de interfaces, mobile first, responsividade, consumo de api's, git, github, entre outros.
+            Atualmente sou desenvolvedor web front end e estou cursando o 2°
+            semestre de Análise e Desenvolvimento de Sistemas na Universidade
+            Anhanguera. Tenho como objetivo me tornar um desenvolvedor full
+            stack, e estou sempre buscando novos conhecimentos para alcançar o
+            que almejo. Atualmente desenvolvo em Javascript/TS e como principal
+            framework utilizo React JS, tenhos conhecimentos em estilizadores
+            css, scss, styled-components, desenvolvimento de interfaces, mobile
+            first, responsividade, consumo de api's, git, github, entre outros.
           </p>
         </div>
       </Styled.ThirdSection>
